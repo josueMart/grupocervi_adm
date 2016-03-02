7869b49a509796af3235455c6eedb318cdb3400f
josueMart <josue.mart@live.com.mx>
2016-03-02 16:11:01 -0600

    Creo el controlador de la vista para el modulo de clientes, queda pendiente diseñar la plantilla base para toda la aplpicación

--- djangoSystemCervi/clientes/views.py
+++ djangoSystemCervi/clientes/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
+from .models import Cliente
 
+def adminClient(request):
+	cliente = Cliente.objects.order_by('id')
+	template = loader.get_template('index.html')
+	context = {
+		'cliente': cliente
+	}
+	return HttpResponse(template.render(context, request))
 # Create your views here.
